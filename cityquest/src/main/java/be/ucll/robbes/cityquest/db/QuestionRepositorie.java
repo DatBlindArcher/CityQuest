44b654b0f9edc5865c23a1cec06ea8587c7e4e67
RobbeCuyvers-r0673295 <robbe.cuyvers@student.ucll.be>
2018-11-06 12:02:57 +0100

    fix

--- cityquest/src/main/java/be/ucll/robbes/cityquest/db/QuestionRepositorie.java
+++
@@ -1,9 +0,0 @@
-package be.ucll.robbes.cityquest.db;
-
-import be.ucll.robbes.cityquest.model.Question;
-import org.springframework.data.repository.CrudRepository;
-
-import java.util.UUID;
-
-public interface QuestionRepositorie extends CrudRepository<Question, UUID> {
-}
