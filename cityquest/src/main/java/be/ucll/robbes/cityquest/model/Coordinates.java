61d3f934b56cd22f2efbe5ea283b0a27e2f2f809
RobbeCuyvers-r0673295 <robbe.cuyvers@student.ucll.be>
2018-11-06 10:15:47 +0100

    coordinates

--- cityquest/src/main/java/be/ucll/robbes/cityquest/model/Coordinates.java
+++ cityquest/src/main/java/be/ucll/robbes/cityquest/model/Coordinates.java
@@ -1,4 +1,27 @@
 package be.ucll.robbes.cityquest.model;
 
 public class Coordinates {
+    private double lat;
+    private double lon;
+
+    public double getLat() {
+        return lat;
+    }
+
+    public void setLat(double lat) {
+        this.lat = lat;
+    }
+
+    public double getLon() {
+        return lon;
+    }
+
+    public void setLon(double lon) {
+        this.lon = lon;
+    }
+
+    public Coordinates(double lat, double lon) {
+        setLat(lat);
+        setLon(lon);
+    }
 }
