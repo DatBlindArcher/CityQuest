d5058a69f963b2e39c630ad2bef92969e1767a36
robinhood128 <robbedecraemer@hotmail.com>
2018-11-16 02:32:14 +0100

    Added frontend

---
+++ Frontend/scripts/cityquest.js
@@ -0,0 +1,52 @@
+/// --- Start --- ///
+
+$(document).ready(function() {
+    register_template("game", "game-template");
+    get_games();
+});
+
+function get_games() {
+    $.ajax({
+        method: "GET",
+        crossDomain: true,
+        url: "http://localhost:8080/games",
+        dataType: 'json',
+        success: function(data) { 
+            console.log("Success");
+            var html = "";
+
+            for (var i = 0; i < data.length; i++) {
+                html += get_template("game", [
+                    { key: "city",          value: data[i].city.name }, 
+                    { key: "lat",           value: "Lat: " + data[i].city.coordinates.lat }, 
+                    { key: "lon",           value: "Lon: " + data[i].city.coordinates.lon }, 
+                    { key: "description",   value: "Yet to be implemented" /*data[i].description*/ }
+                ]);
+            }
+
+            $("#games").html(html);
+        },
+        error: function(data) { console.log("Success"); $("#games").html(data); }
+    });
+}
+
+/// ---- Templates ---- ///
+
+let templates = {};
+
+function register_template(template_name, template_id) {
+    var element = document.getElementById(template_id);
+    templates[template_name] = element.innerHTML;
+    element.parentNode.removeChild(element);
+}
+
+function get_template(template_name, key_value_pairs) {
+    var html = templates[template_name];
+
+    for (var i = 0; i < key_value_pairs.length; ++i) {
+        var regex = new RegExp("{" + key_value_pairs[i].key + "}", "g");
+        html = html.replace(regex, key_value_pairs[i].value);
+    }
+
+    return html;
+}
--- cityquest/src/main/java/be/ucll/robbes/cityquest/CityquestApplication.java
+++ cityquest/src/main/java/be/ucll/robbes/cityquest/CityquestApplication.java
@@ -2,11 +2,26 @@ package be.ucll.robbes.cityquest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class CityquestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CityquestApplication.class, args);
+	}
+
+
+	/// Why: Otherwise we get a Access-Control-Allow-Origin error
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*");
+			}
+		};
 	}
 }
