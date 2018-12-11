e8b97c12667f28f2a653775a124772d19d785cf1
RobbeCuyvers-r0673295 <robbe.cuyvers@student.ucll.be>
2018-12-11 11:38:06 +0100

    leaderboard werkt

---
+++ leaderboard/src/main/java/be/ucll/robbes/leaderboard/CityquestResultApplication.java
@@ -0,0 +1,27 @@
+package be.ucll.robbes.leaderboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootApplication
+public class CityquestResultApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CityquestResultApplication.class, args);
+    }
+
+
+    /// Why: Otherwise we get a Access-Control-Allow-Origin error
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")/*.allowedOrigins("*")*/;
+            }
+        };
+    }
+}
